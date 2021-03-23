ab86cbf4d4ac953a23f90ac6f74e84f58e27b95b
marci543 <marci543@users.noreply.github.com>
2021-03-23 22:26:58 +0100

    Suppress missing AHT components, exit after startup

--- demo-car/demo-car-provider/src/main/java/eu/arrowhead/client/skeleton/provider/ProviderApplicationInitListener.java
+++ demo-car/demo-car-provider/src/main/java/eu/arrowhead/client/skeleton/provider/ProviderApplicationInitListener.java
@@ -11,11 +11,14 @@ import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 
+import eu.arrowhead.common.exception.UnavailableServerException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
@@ -34,71 +37,87 @@ import eu.arrowhead.common.exception.ArrowheadException;
 
 @Component
 public class ProviderApplicationInitListener extends ApplicationInitListener {
-	
+
 	//=================================================================================================
 	// members
-	
+
 	@Autowired
 	private ArrowheadService arrowheadService;
-	
+
 	@Autowired
 	private ProviderSecurityConfig providerSecurityConfig;
-	
+
 	@Value(ClientCommonConstants.$TOKEN_SECURITY_FILTER_ENABLED_WD)
 	private boolean tokenSecurityFilterEnabled;
-	
+
 	@Value(CommonConstants.$SERVER_SSL_ENABLED_WD)
 	private boolean sslEnabled;
-	
+
 	@Value(ClientCommonConstants.$CLIENT_SYSTEM_NAME)
 	private String mySystemName;
-	
+
 	@Value(ClientCommonConstants.$CLIENT_SERVER_ADDRESS_WD)
 	private String mySystemAddress;
-	
+
 	@Value(ClientCommonConstants.$CLIENT_SERVER_PORT_WD)
 	private int mySystemPort;
-	
+
 	private final Logger logger = LogManager.getLogger(ProviderApplicationInitListener.class);
-	
+
 	//=================================================================================================
 	// methods
+
+    @Autowired
+    private TaskExecutor taskExecutor;
 
 	//-------------------------------------------------------------------------------------------------
 	@Override
 	protected void customInit(final ContextRefreshedEvent event) {
+	    try {
+            //Checking the availability of necessary core systems
+            checkCoreSystemReachability(CoreSystem.SERVICE_REGISTRY);
+            if (sslEnabled && tokenSecurityFilterEnabled) {
+                checkCoreSystemReachability(CoreSystem.AUTHORIZATION);
 
-		//Checking the availability of necessary core systems
-		checkCoreSystemReachability(CoreSystem.SERVICE_REGISTRY);
-		if (sslEnabled && tokenSecurityFilterEnabled) {
-			checkCoreSystemReachability(CoreSystem.AUTHORIZATION);			
+                //Initialize Arrowhead Context
+                arrowheadService.updateCoreServiceURIs(CoreSystem.AUTHORIZATION);
 
-			//Initialize Arrowhead Context
-			arrowheadService.updateCoreServiceURIs(CoreSystem.AUTHORIZATION);
-			
-			setTokenSecurityFilter();
-		} else {
-			logger.info("TokenSecurityFilter in not active");
-		}		
-		
-		
-		//Register services into ServiceRegistry
-		final ServiceRegistryRequestDTO createCarServiceRequest = createServiceRegistryRequest(CarProviderConstants.CREATE_CAR_SERVICE_DEFINITION, CarProviderConstants.CAR_URI, HttpMethod.POST);		
-		arrowheadService.forceRegisterServiceToServiceRegistry(createCarServiceRequest);
-		
-		ServiceRegistryRequestDTO getCarServiceRequest = createServiceRegistryRequest(CarProviderConstants.GET_CAR_SERVICE_DEFINITION,  CarProviderConstants.CAR_URI, HttpMethod.GET);
-		getCarServiceRequest.getMetadata().put(CarProviderConstants.REQUEST_PARAM_KEY_BRAND, CarProviderConstants.REQUEST_PARAM_BRAND);
-		getCarServiceRequest.getMetadata().put(CarProviderConstants.REQUEST_PARAM_KEY_COLOR, CarProviderConstants.REQUEST_PARAM_COLOR);
-		arrowheadService.forceRegisterServiceToServiceRegistry(getCarServiceRequest);
+                setTokenSecurityFilter();
+            } else {
+                logger.info("TokenSecurityFilter in not active");
+            }
+
+
+            //Register services into ServiceRegistry
+            final ServiceRegistryRequestDTO createCarServiceRequest = createServiceRegistryRequest(CarProviderConstants.CREATE_CAR_SERVICE_DEFINITION, CarProviderConstants.CAR_URI, HttpMethod.POST);
+            arrowheadService.forceRegisterServiceToServiceRegistry(createCarServiceRequest);
+
+            ServiceRegistryRequestDTO getCarServiceRequest = createServiceRegistryRequest(CarProviderConstants.GET_CAR_SERVICE_DEFINITION,  CarProviderConstants.CAR_URI, HttpMethod.GET);
+            getCarServiceRequest.getMetadata().put(CarProviderConstants.REQUEST_PARAM_KEY_BRAND, CarProviderConstants.REQUEST_PARAM_BRAND);
+            getCarServiceRequest.getMetadata().put(CarProviderConstants.REQUEST_PARAM_KEY_COLOR, CarProviderConstants.REQUEST_PARAM_COLOR);
+            arrowheadService.forceRegisterServiceToServiceRegistry(getCarServiceRequest);
+        } catch (UnavailableServerException exception) {
+            // to test without the presence of AHT framework
+            exception.printStackTrace();
+        }
+        // exit after some time
+        taskExecutor.execute(() -> {
+            try {
+                Thread.sleep(1_000);
+                ((ConfigurableApplicationContext) event.getApplicationContext()).close();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
 	}
-	
+
 	//-------------------------------------------------------------------------------------------------
 	@Override
 	public void customDestroy() {
 		//Unregister service
 		arrowheadService.unregisterServiceFromServiceRegistry(CarProviderConstants.CREATE_CAR_SERVICE_DEFINITION);
 	}
-	
+
 	//=================================================================================================
 	// assistant methods
 
@@ -108,21 +127,21 @@ public class ProviderApplicationInitListener extends ApplicationInitListener {
 		if (authorizationPublicKey == null) {
 			throw new ArrowheadException("Authorization public key is null");
 		}
-		
+
 		KeyStore keystore;
 		try {
 			keystore = KeyStore.getInstance(sslProperties.getKeyStoreType());
 			keystore.load(sslProperties.getKeyStore().getInputStream(), sslProperties.getKeyStorePassword().toCharArray());
 		} catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException ex) {
 			throw new ArrowheadException(ex.getMessage());
-		}			
+		}
 		final PrivateKey providerPrivateKey = Utilities.getPrivateKey(keystore, sslProperties.getKeyPassword());
-		
+
 		providerSecurityConfig.getTokenSecurityFilter().setAuthorizationPublicKey(authorizationPublicKey);
 		providerSecurityConfig.getTokenSecurityFilter().setMyPrivateKey(providerPrivateKey);
 
 	}
-	
+
 	//-------------------------------------------------------------------------------------------------
 	private ServiceRegistryRequestDTO createServiceRegistryRequest(final String serviceDefinition, final String serviceUri, final HttpMethod httpMethod) {
 		final ServiceRegistryRequestDTO serviceRegistryRequest = new ServiceRegistryRequestDTO();
@@ -130,7 +149,7 @@ public class ProviderApplicationInitListener extends ApplicationInitListener {
 		final SystemRequestDTO systemRequest = new SystemRequestDTO();
 		systemRequest.setSystemName(mySystemName);
 		systemRequest.setAddress(mySystemAddress);
-		systemRequest.setPort(mySystemPort);		
+		systemRequest.setPort(mySystemPort);
 
 		if (tokenSecurityFilterEnabled) {
 			systemRequest.setAuthenticationInfo(Base64.getEncoder().encodeToString(arrowheadService.getMyPublicKey().getEncoded()));
